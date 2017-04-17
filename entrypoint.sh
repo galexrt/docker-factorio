ceb5d27c328349b4ee1a204374888000f093df06
Alexander Trost <galexrt@googlemail.com>
2017-04-17 19:44:34 +0200

    Fix the exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 mkdir -p /data/saves
 ln -s /data/saves /factorio/saves || { echo "Failed linking saves directory."; exit 1; }
 
-exec gosu $USER_ID:$GROUP_ID -- "$@"
+exec gosu $USER_ID:$GROUP_ID "$@"
